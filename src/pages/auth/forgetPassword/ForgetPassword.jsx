7a1f5da48e6a6598ff2004ce0aea9f9f6b4aa4f4
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-10-19 23:38:44 +0700

    update

--- src/pages/auth/forgetPassword/ForgetPassword.jsx
+++ src/pages/auth/forgetPassword/ForgetPassword.jsx
@@ -1,26 +1,33 @@
 /* eslint-disable */
 import React from 'react';
 import './ForgetPassword.css';
+import Header from '@/components/header/Header';
+import { Field, Form, Formik } from 'formik';
 
 const ForgetPassword = () => {
   return (
-    <div className="container-forgotPassword">
-      <main className="main-content">
-        <div className="form-card">
-          <h2 className="form-title">QUÊN MẬT KHẨU</h2>
+    <div >
+      <Header/>
+      <Formik>
+        <Form>
+          <div className="main-content">
+            <div className="form-card">
+              <h2 className="form-title">QUÊN MẬT KHẨU</h2>
               <form>
                 <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="username">Tên đăng nhập</label>
-                <input type="text" id="username" name="username" />
-              </div>
-              <button type="submit" className="submit-btn">Gửi lại mật khẩu</button>
-            </form>
-        </div>
-      </main>
+                  <label htmlFor="email">Email</label>
+                  <Field type="email" id="email" name="email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="username">Tên đăng nhập</label>
+                  <Field type="text" id="username" name="username" />
+                </div>
+                <button type="submit" className="submit-btn">Gửi lại mật khẩu</button>
+              </form>
+            </div>
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 };
--- src/pages/auth/resetPassword/ResetPassword.jsx
+++ src/pages/auth/resetPassword/ResetPassword.jsx
@@ -3,33 +3,40 @@ import React from 'react'
 
 import './ResetPassword.css';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
+import Header from '@/components/header/Header';
 
 const ResetPassword = () => {
   return (
- <div className="ResetPasswordcontent">
-    <Formik>
-      <Form>
-        <h1>Đổi Mật Khẩu</h1>
-        <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <Field type="email" id="email" name="email" />
-              <ErrorMessage name="email" component="div" className="error" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="username">Tên Đăng Nhập</label>
-              <Field type="text" id="username" name="username" />
-              <ErrorMessage name="username" component="div" className="error" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="newPassword">Mật Khẩu Mới</label>
-              <Field type="password" id="newPassword" name="newPassword" />
-              <ErrorMessage name="newPassword" component="div" className="error" />
-            </div>
-            <button type="submit">Đổi Mật Khẩu</button>
-      </Form>
-    </Formik>
-  </div>
- )
- }
+    <>
+      <Header />
+
+      <div className='resetPasswordBody'>
+        <div className="ResetPasswordcontent">
+          <Formik>
+            <Form>
+              <h1 className='reset-name'>Đổi Mật Khẩu</h1>
+              <div className="reset-form-group">
+                <label htmlFor="email" className='reset-label'>Email</label>
+                <Field type="email" id="email" name="email" className='reset-input'/>
+                <ErrorMessage name="email" component="div" className="error" />
+              </div>
+              <div className="reset-form-group">
+                <label htmlFor="username" className='reset-label'>Tên Đăng Nhập</label>
+                <Field type="text" id="username" name="username" className='reset-input' />
+                <ErrorMessage name="username" component="div" className="error" />
+              </div>
+              <div className="reset-form-group">
+                <label htmlFor="newPassword" className='reset-label'>Mật Khẩu Mới</label>
+                <Field type="password" id="newPassword" name="newPassword" className='reset-input' />
+                <ErrorMessage name="newPassword" component="div" className="error" />
+              </div>
+              <button type="submit" className='reset-btn'>Đổi Mật Khẩu</button>
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default ResetPassword;
