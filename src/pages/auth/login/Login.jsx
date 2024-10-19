c04cabab4831efde20a9ecfee8310a5af052f424
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-10-20 00:13:48 +0700

    update style login

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -4,51 +4,55 @@ import React from 'react'
 import './Login.css';
 import { Field, Form, Formik } from 'formik';
 import hinhnen from '../../../assets/images/hinhnen.jpg'
+import Header from '@/components/header/Header';
 
 const Login = () => {
 
   return (
-    <div>
-      <Formik>
-        <Form>
-          <div className='login-container'>
-            <div className='login-left'>
-              <img src={hinhnen} alt="" />
-            </div>
-            <div className='login-right'>
-              <div className='login-h1'>
-                Đăng nhập vào tài khoản
-              </div>
-
-              <div className='login-un'>
-                <label htmlFor="userName" className='login-label'>Tên đăng nhập: </label>
-                <Field name='userName' className='login-input' type='text'/>
-              </div>
-
-              <div className='login-pw'>
-                <label htmlFor="password" className='login-label' >Mật khẩu: </label>
-                <Field name='password' className='login-input' />
-              </div>
-
-              <div className='login-s3'>
-                <div className='rememberPW'>
-                  <input type="checkbox" name='recomendPassword' id='login-radio'/>
-                  <label htmlFor="recomendPassword" className='login-label'>Ghi nhớ tôi</label>
-                </div>
-
-                <div className='forgetPW'>
-                  <a href="#">Quên mật khẩu?</a>
-                </div>
-              </div>
-
-              <div className='login-btn'>
-                  <button type="submit" id='loginBTN'>Đăng nhập</button>
-              </div>
-            </div>
+    <>
+      <Header />
+      <div className='login-body'>
+        <div className='login-contain'>
+          <div className='login-left'>
+            <img src={hinhnen} alt="" className='login-image' />
           </div>
-        </Form>
-      </Formik>
-    </div>
+          <div className='login-right'>
+            <Formik>
+              <Form>
+                <div className='login-h1'>
+                  Đăng nhập vào tài khoản
+                </div>
+
+                <div className='login-group'>
+                  <label htmlFor="userName" className='login-label'>Tên đăng nhập: </label>
+                  <Field name='userName' className='login-input' type='text' />
+                </div>
+
+                <div className='login-group'>
+                  <label htmlFor="password" className='login-label' >Mật khẩu: </label>
+                  <Field name='password' className='login-input' />
+                </div>
+
+                <div className='login-s3'>
+                  <div className='rememberPW'>
+                    <input type="checkbox" name='recomendPassword' id='login-radio' />
+                    <label htmlFor="recomendPassword" className='login-label'>Ghi nhớ tôi</label>
+                  </div>
+
+                  <div className='forgetPW'>
+                    <p href="#" className='foget-text'>Quên mật khẩu?</p>
+                  </div>
+                </div>
+
+                <div className='login-btn'>
+                  <button type="submit" id='loginBTN'>Đăng nhập</button>
+                </div>
+              </Form>
+            </Formik>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
