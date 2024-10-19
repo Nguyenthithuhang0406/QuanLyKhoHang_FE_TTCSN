f288e15b312aa7cd541230c958692b9f0e098122
vhiep04 <vhiep604@gmail.com>
2024-10-19 20:41:39 +0700

    init

--- src/pages/auth/forgetPassword/ForgetPassword.jsx
+++ src/pages/auth/forgetPassword/ForgetPassword.jsx
@@ -1,12 +1,28 @@
 /* eslint-disable */
-import React from 'react'
-
+import React from 'react';
 import './ForgetPassword.css';
 
 const ForgetPassword = () => {
   return (
-    <div>ForgetPassword</div>
-  )
-}
+    <div className="container">
+      <main className="main-content">
+        <div className="form-card">
+          <h2 className="form-title">QUÊN MẬT KHẨU</h2>
+              <form>
+                <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="username">Tên đăng nhập</label>
+                <input type="text" id="username" name="username" />
+              </div>
+              <button type="submit" className="submit-btn">Gửi lại mật khẩu</button>
+            </form>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default ForgetPassword;
