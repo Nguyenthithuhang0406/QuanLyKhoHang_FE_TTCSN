3e88ee2e58a5b380cf85c704016bc78254d9cd44
DuyDuc1801 <pdduc18012004@gmail.com>
2024-10-23 20:16:23 +0700

    .

--- src/pages/product/createdProduct/CreatedProduct.jsx
+++ src/pages/product/createdProduct/CreatedProduct.jsx
@@ -2,11 +2,25 @@
 import React from 'react'
 
 import './CreatedProduct.css';
+import Header from '@/components/header/Header';
+import NavBar from '@/components/navBar/NavBar';
 
 const CreatedProduct = () => {
+
   return (
-    <div>
-      
+
+    <div className="createProduct-container"> 
+    <Header className="header-cP"/>
+    <NavBar/>
+      <div className="createProduct-form">
+        <div className="createProduct-title">Thêm hàng hóa</div>
+        <div className="main-form">
+          <div className="productID-row">
+            <label htmlFor="productID" className="createProduct-lbl">Mã hàng</label>
+            <input name="productID" className="createProduct-txt" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
