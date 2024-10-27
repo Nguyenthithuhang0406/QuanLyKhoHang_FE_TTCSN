f713d5525938c0315e13b412f3b171e17e8e7643
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-10-27 12:40:56 +0700

    call API supply

---
+++ src/api/suppliesAPI/supply.js
@@ -0,0 +1,110 @@
+import { requestWithToken } from "@/utils/axios-http";
+
+
+export const createdSupply = async (data) => {
+  const { code, name, address, phone, email, respresentative, type } = data;
+  try {
+    const supply = await requestWithToken({
+      url: "/createSupplies",
+      method: "post",
+      data: {
+        code,
+        name,
+        address,
+        phone,
+        email,
+        respresentative,
+        type,
+      },
+    });
+
+    return supply.data.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updatedSupply = async (data, supplyId) => {
+  const { code, name, address, phone, email, respresentative, type } = data;
+  try {
+    const updateSupply = await requestWithToken({
+      url: `/updateSupplies/${supplyId}`,
+      method: "put",
+      data: {
+        code,
+        name,
+        address,
+        phone,
+        email,
+        respresentative,
+        type,
+      },
+    });
+
+    return updateSupply.data.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export const deleteSupply = async (type, supplyId) => {
+  try {
+    await requestWithToken({
+      url: `/deleteSupplies/${supplyId}`,
+      method: "delete",
+      data: {
+        type,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const getSupplyById = async (type, supplyId) => {
+  try {
+    const supply = await requestWithToken({
+      url: `/getSupplyById/${supplyId}`,
+      method: "get",
+      data: {
+        type,
+      },
+    });
+
+    return supply.data.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export const getSupplies = async (limit, page) => {
+  try {
+    const supplies = await requestWithToken({
+      url: `/getSupplies?page=${page}&limit=${limit}`,
+      method: "get",
+    });
+
+    return supplies.data.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export const searchSupply = async (code = "", name = "", phone = "", type, page, limit) => {
+  try {
+    const supplies = await requestWithToken({
+      url: `/searchSupply?code=${code}&name=${name}&phone=${phone}&type=${type}&page=${page}&limit=${limit}`,
+      method: "get",
+    });
+
+    return supplies.data.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
